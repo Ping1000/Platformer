267c12ee11133fb05f22a445f14cb11c08600980
Dakota Hernandez <46770478+dhernan881@users.noreply.github.com>
2021-03-13 02:04:01 -0500

    Added BASIC time swapping

---
+++ Ping1000 Platformer/Assets/Scripts/TimeManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    public KeyCode timeSwitch;
+    public GameObject pastParent;
+    public GameObject pastTilemap;
+    public GameObject presentParent;
+    public GameObject presentTilemap;
+
+    public bool isPast;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isPast = false;
+        SetTime(isPast);
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(timeSwitch)) {
+            SwapTime();
+        }
+    }
+
+    public void SwapTime() {
+        isPast = !isPast;
+        SetTime(isPast);
+    }
+
+    public void SetTime(bool isPast) {
+        pastParent.SetActive(isPast);
+        pastTilemap.SetActive(isPast);
+        presentParent.SetActive(!isPast);
+        presentTilemap.SetActive(!isPast);
+    }
+}
