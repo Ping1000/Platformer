85e060ccad93aa434da74d3257506eebcca8afe3
Dakota Hernandez <46770478+dhernan881@users.noreply.github.com>
2021-04-05 04:52:29 -0400

    Added basic boss cutscene
    Add objects you want disabled during the boss cutscene to the lists in the BossFightStart script on the Fungus Flowchart

---
+++ Ping1000 Platformer/Assets/Scripts/Boss/BossFightStart.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossFightStart : MonoBehaviour
+{
+    public List<GameObject> objectsToActivate;
+    public List<MonoBehaviour> componentsToEnable;
+
+    private void Start() {
+        foreach (GameObject go in objectsToActivate) {
+            go.SetActive(false);
+        }
+        foreach (MonoBehaviour mb in componentsToEnable) {
+            mb.enabled = false;
+        }
+    }
+
+    public void StartFight() {
+        foreach (GameObject go in objectsToActivate) {
+            go.SetActive(true);
+        }
+        foreach (MonoBehaviour mb in componentsToEnable) {
+            mb.enabled = true;
+        }
+    }
+}
